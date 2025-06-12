2fe36f9b149e6f6c3049e9b617c0781535fe3c2b
bdj34 <bdj001@ucsd.edu>
2025-06-12 14:40:41 -0700

    Modify bash to run completeness

--- brian_testing/run_completeness.sh
+++ brian_testing/run_completeness.sh
@@ -3,14 +3,14 @@
 cd ~/llama.cpp_2025_06_10
 
 ./build/bin/llama-data-extraction \
--m ~/media/disk/_cygdrive_P_ORD_Curtius_202210036D_nlp_v2/models_gguf/medgemma-27b-text-it-Q4KM.gguf \
+-m ~/media/disk/_cygdrive_P_ORD_Curtius_202210036D_nlp_v2/models_gguf/medgemma-4b-it-Q80.gguf \
 -sysf ./system_prompts/gemma2/completenessOfResection.txt \
---no-escape \
---sequences $(wc -l < ../completeness/inputs/input.txt) \
---parallel 1 --n-predict 30 --batch-size 2048 --n-gpu-layers 0 --ctx-size 4096 \
+--no-escape --swa-full \
+--sequences $(wc -l < ~/completeness/inputs/inputs.txt) \
+--parallel 4 --n-predict 30 --batch-size 2048 --n-gpu-layers 0 --ctx-size 16384 \
 --temp 0 \
---IDfile ../completeness/inputs/sid_and_sample.txt \
+--IDfile ~/completeness/inputs/sid_and_sample.txt \
 --grammar-file ./grammars/completenessOfResection.gbnf \
---outDir ../completeness/results \
---file ../completeness/inputs/input.txt \
+--outDir ~/completeness/results \
+--file ~/completeness/inputs/inputs.txt \
 --promptFormat gemma2
