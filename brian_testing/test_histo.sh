1e26d66a84ae05496e2bcbe1cf572990ae784272
bdj34 <bdj001@ucsd.edu>
2026-01-20 10:37:47 -0800

    Modifications to grammar and testing file after usccessful testing of histo colitis prompt

--- brian_testing/test_histo.sh
+++ brian_testing/test_histo.sh
@@ -1,11 +1,12 @@
 # Med Gemma 3 27B
 cd /Users/brianjohnson/VA_IBD/llama.cpp_dev_merge_public_20251111
 ./build/bin/llama-data-extraction \
--m ~/Downloads/models_gguf/medgemma-27b-text-it-Q6_K.gguf \
+-m ~/Downloads/models_gguf/medgemma-27b-text-it-Q4_K_M.gguf \
 -sysf ./system_prompts/gemma/histologic_colitis.txt \
 --no-escape --swa-full \
 --sequences 5 --parallel 1 --n-predict 300 --batch-size 2048 --n-gpu-layers 99 --ctx-size 8192 \
 --temp 0 --saveInput \
+--grammar-file ./grammars/histologic_colitis.gbnf \
 --IDfile /Users/brianjohnson/VA_IBD/testing_data/histo_colitis/IDs.txt \
 --outDir ../testing_data/histo_colitis/output \
 --file /Users/brianjohnson/VA_IBD/testing_data/histo_colitis/inputs.txt \
